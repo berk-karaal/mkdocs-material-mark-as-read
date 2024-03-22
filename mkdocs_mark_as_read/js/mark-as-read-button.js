82709d78e2fd733f2756d3be61a597d65d0cc442
Berk Karaal <karaalberk7@gmail.com>
2024-03-22 17:58:57 +0300

    refactor(mark-as-read-button.js): clean code

--- mkdocs_mark_as_read/js/mark-as-read-button.js
+++ mkdocs_mark_as_read/js/mark-as-read-button.js
@@ -3,31 +3,40 @@ function markAsReadGetStorageKey() {
   return `read-${window.location.pathname}`;
 }
 
-function markAsReadButtonOnClick() {
-  let storage_key = markAsReadGetStorageKey();
-
-  let read_at = window.localStorage.getItem(storage_key);
+/** Update button text based on read data. */
+function updateMarkAsReadButtonText() {
   let button = document.getElementsByClassName("mark-as-read-button")[0];
+  let storage_key = markAsReadGetStorageKey();
+  let read_at = window.localStorage.getItem(storage_key);
+
+  if (read_at === null) {
+    button.innerHTML = "Mark as read";
+  } else {
+    button.innerHTML = "Mark as unread";
+  }
+}
+
+/** Update read data on local storage.
+ * If data is not present, set it to current time. Otherwise, remove it.
+ */
+function updateMarkAsReadData() {
+  let storage_key = markAsReadGetStorageKey();
+  let read_at = window.localStorage.getItem(storage_key);
 
   if (read_at === null) {
     window.localStorage.setItem(storage_key, new Date().toISOString());
-    button.innerHTML = "Mark as unread";
   } else {
     window.localStorage.removeItem(storage_key);
-    button.innerHTML = "Mark as read";
   }
+}
 
+/** Button click handler */
+function markAsReadButtonOnClick() {
+  updateMarkAsReadData();
+  updateMarkAsReadButtonText();
+
+  // markAsReadNavUpdater is declared by plugin script on load.
   markAsReadNavUpdater.updateRelatedNavLink(window.location.pathname);
 }
 
-function initButtonText() {
-  let storage_key = markAsReadGetStorageKey();
-  let read_at = window.localStorage.getItem(storage_key);
-  let button = document.getElementsByClassName("mark-as-read-button")[0];
-  if (read_at === null) {
-    button.innerHTML = "Mark as read";
-  } else {
-    button.innerHTML = "Mark as unread";
-  }
-}
-initButtonText();
+updateMarkAsReadButtonText();
